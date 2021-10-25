b120b8a5987f4a2565e3149401a68dbb6be6b9c8
skytomo <tomo1kawa.fullerene@gmail.com>
2021-10-25 11:53:40 +0900

    Use unicode-blocks and unicode-general-category

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,16 @@
 
 use encoding::all::ISO_2022_JP;
 use encoding::{EncoderTrap, Encoding};
-use wana_kana::is_kanji::is_kanji;
+use unicode_blocks::{
+    find_unicode_block, CJK_COMPATIBILITY, ENCLOSED_CJK_LETTERS_AND_MONTHS, HIRAGANA, KATAKANA,
+};
+use unicode_general_category::{get_general_category, GeneralCategory};
+
+fn can_encode_jis_code(c: char) -> bool {
+    ISO_2022_JP
+        .encode(&c.to_string(), EncoderTrap::Strict)
+        .is_ok()
+}
 
 fn main() {
     for rhyme in 0x80..=0xBF {
@@ -10,13 +19,15 @@ fn main() {
         for i in 0x30..=0x9F {
             for j in [0, 1, 2, 3] {
                 let c = char::from_u32(i * 0x100 + j * 0x40 + (rhyme - 0x80)).unwrap();
-                let s = &c.to_string();
-                if is_kanji(s) && ISO_2022_JP.encode(s, EncoderTrap::Strict).is_ok() {
-                    print!("{}", c)
-                } else if !('\u{3130}' <= c && c <= '\u{31ef}')
-                    && !('㈀' <= c && c <= '㈞')
-                    && !('㉠' <= c && c <= '㉾')
-                    && !('\u{3300}' <= c && c <= '\u{9fff}')
+                let block = find_unicode_block(c).unwrap();
+                if get_general_category(c) != GeneralCategory::Unassigned
+                    && (can_encode_jis_code(c)
+                        || block == HIRAGANA
+                        || block == KATAKANA
+                        || (block == ENCLOSED_CJK_LETTERS_AND_MONTHS
+                            && !('㈀' <= c && c <= '㈞')
+                            && !('㉠' <= c && c <= '㉾'))
+                        || block == CJK_COMPATIBILITY)
                 {
                     print!("{}", c)
                 }
